8fc1695804699b7edfdf2c2833fb0bdac074de90
tohbey <fafoworatobi25@gmail.com>
2022-01-21 00:53:42 +0100

    adding a default route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {
   BrowserRouter as Router,
-  Route, Routes, 
+  Navigate,
+  Route, Routes,
 } from "react-router-dom";
 import React from 'react';
 import Login from '../src/pages/auth/login';
 import Signup from '../src/pages/auth/signup';
 import Validation from '../src/pages/auth/validation';
 import ForgotPassword from '../src/pages/auth/forgot-password';
+import Verification from '../src/pages/auth/verification';
 
 
 function App() {
@@ -16,7 +18,9 @@ function App() {
           <Route path='/login' exact element={<Login />} />
           <Route path='/signup' exact element={<Signup />} />
           <Route path='/validation' exact element={<Validation />} />
+          <Route path='/verify' exact element={<Verification />} />
           <Route path='/forgot-Password' exact element={<ForgotPassword />} />
+          <Route path='' element={<Navigate to="login" />} />
       </Routes>
     </Router>
   );

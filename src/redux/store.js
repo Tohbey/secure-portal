556d6f46242f74342a75f35fd4e95e9bfe6469ee
tohbey <fafoworatobi25@gmail.com>
2022-01-21 00:09:30 +0100

    store update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import thunk from 'redux-thunk'
-import { createStore, applyMiddleware, combineReducers} from 'redux'
-
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { UserReducer } from './reducers/user';
+import { AuthReducer } from './reducers/auth';
 
 const rootReducer = combineReducers({
-
+    user: UserReducer, 
+    auth: AuthReducer
 })
 
-const configureStore = () =>  createStore(rootReducer, applyMiddleware(thunk))
+const configureStore = () => createStore(rootReducer, applyMiddleware(thunk))
 
 export default configureStore;

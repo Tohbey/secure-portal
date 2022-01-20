ad2e0037aaff3f275517189a0241e32d6cc7a5e4
tohbey <fafoworatobi25@gmail.com>
2022-01-21 00:09:57 +0100

    user and auth reducers

---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,79 @@
+import {
+    FETCH_REQUEST,
+    FETCH_FAILURE,
+    LOGIN_SUCCESS,
+    SECOND_AUTH_SUCCESS,
+    VERIFY_SUCCESS,
+    RESEND_TOKEN_SUCCESS,
+    RECOVERY_SUCCESS,
+    RESET_PASSWORD_SUCCESS,
+    LOGOUT
+} from '../actionTypes/auth';
+
+const initialState = {
+    loading: false,
+    error: '',
+    isError: false,
+    isLoggedIn: false,
+    secondAuth: false,
+};
+
+
+export const AuthReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case FETCH_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+                isError: true
+            }
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isLoggedIn: true,
+                error: '',
+                isError: false
+            }
+        case SECOND_AUTH_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                secondAuth: true,
+                error: '',
+                isError: false
+            }
+        case VERIFY_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+            }
+        case RESEND_TOKEN_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+            }
+        case RECOVERY_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+            }
+        case RESET_PASSWORD_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+            }
+        case LOGOUT:
+            return {
+                ...state,
+                loading: false,   
+            }
+        default: return state
+    }
+}
---
+++ src/redux/reducers/user.js
@@ -0,0 +1,76 @@
+import {
+    REGISTER_SUCCESS,
+    REQUEST,
+    REQUEST_FAILURE,
+    GET_USER,
+    GET_CURRENT_USER,
+    GET_USERS,
+    TERMINATE_USER_SUCCESS
+} from '../actionTypes/user';
+
+const initialState = {
+    loading: false,
+    error: "",
+    isError: false,
+    currentUser: null,
+    newUser: false,
+    users: []
+};
+
+export const UserReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case REQUEST_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                isError: true,
+                error: action.payload
+            }
+        case REGISTER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: '',
+                newUser: true,
+                isError: false,
+            }
+        case GET_USERS:
+            return {
+                ...state,
+                loading: false,
+                users: action.payload,
+                error: '',
+                isError: false,
+            }
+        case GET_CURRENT_USER:
+            return {
+                ...state,
+                loading: false,
+                currentUser: action.payload,
+                error: '',
+                isError: false,
+            }
+        case GET_USER:
+            return {
+                ...state,
+                loading: false,
+                user: action.payload,
+                error: '',
+                isError: false,
+            }
+        case TERMINATE_USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: '',
+                isError: false,
+            }
+
+        default: return state;
+    }
+}

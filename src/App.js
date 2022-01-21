793b06a67eb5542b8ac822f652feed9371701be1
tohbey <fafoworatobi25@gmail.com>
2022-01-21 16:14:24 +0100

    addtion of extra routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Signup from '../src/pages/auth/signup';
 import Validation from '../src/pages/auth/validation';
 import ForgotPassword from '../src/pages/auth/forgot-password';
 import Verification from '../src/pages/auth/verification';
+import Home from '../src/pages/home/home';
 
 
 function App() {
@@ -16,10 +17,12 @@ function App() {
     <Router>
       <Routes>
           <Route path='/login' exact element={<Login />} />
-          <Route path='/signup' exact element={<Signup />} />
           <Route path='/validation' exact element={<Validation />} />
+          <Route path='/signup' exact element={<Signup />} />
           <Route path='/verify' exact element={<Verification />} />
           <Route path='/forgot-Password' exact element={<ForgotPassword />} />
+          <Route path='/forgot-Password/:email/:token' exact element={<ForgotPassword />} />
+          <Route path='/home' exact element={<Home />} />
           <Route path='' element={<Navigate to="login" />} />
       </Routes>
     </Router>

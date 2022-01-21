0b8a81068bf84ebff127ecc2ce78b2d3341bb103
tohbey <fafoworatobi25@gmail.com>
2022-01-22 00:23:19 +0100

    update

--- src/redux/services/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    const user = JSON.parse(localStorage.getItem('x-auth-token'));
-
-    if (user && user.accessToken) {
-        return { Authorization: 'x-auth-token ' + user.accessToken };
-    } else {
-        return {};
-    }
-}

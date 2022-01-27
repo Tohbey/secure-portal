095c7404ae2b0473e90bb101f5e582bff27ba62a
tohbey <fafoworatobi25@gmail.com>
2022-01-27 15:42:03 +0100

    update in routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ import Validation from '../src/pages/auth/validation';
 import ForgotPassword from '../src/pages/auth/forgot-password';
 import Verification from '../src/pages/auth/verification';
 import Home from '../src/pages/home/home';
-
+import Profile from '../src/pages/profile';
+import SharedDocuments from '../src/pages/shared';
+import ShareDocuments from '../src/pages/share';
+import UploadedDocuments from '../src/pages/uploaded';
 
 function App() {
   return (
@@ -23,6 +26,10 @@ function App() {
           <Route path='/forgot-Password' exact element={<ForgotPassword />} />
           <Route path='/forgot-Password/:email/:token' exact element={<ForgotPassword />} />
           <Route path='/home' exact element={<Home />} />
+          <Route path='/profile' exact element={<Profile />} />
+          <Route path='/share' exact element={<ShareDocuments />} />
+          <Route path='/shared-document' exact element={<SharedDocuments />} />
+          <Route path='/uploaded' exact element={<UploadedDocuments />} />
           <Route path='' element={<Navigate to="login" />} />
       </Routes>
     </Router>

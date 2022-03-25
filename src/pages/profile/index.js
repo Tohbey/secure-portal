b4e3c6ae738a0f8e60862d4b91eb52f79f370b19
tohbey <fafoworatobi25@gmail.com>
2022-03-25 08:03:43 +0100

    profile page update

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Dashboard from '../../container/dashboard';
+import './index.scss'
 
 const profile = () => {
     return (
         <Dashboard>
-            <p>Profile</p>
+            <p className="header">Profile</p>
+            <div className="content">
+                <div>
+                    <p>name</p>
+                </div>
+                <div>
+                    <p>role</p>
+                </div>
+                <div>
+                    <p>email</p>
+                </div>
+                <div>
+                    <p>secret question</p>
+                </div>
+            </div>
         </Dashboard>
     )
 }
